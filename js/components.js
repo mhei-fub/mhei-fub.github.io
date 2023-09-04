a58a2b1fbb5e903981a0ab255076746b999a1ec9
Joaquin Gottlebe <mail@huaqo.com>
2023-09-04 12:39:42 +0200

    Add files via upload

---
+++ js/components.js
@@ -0,0 +1,17 @@
+class NavbarComponent extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="navbar">
+                <button class="arrow" id="Btn">
+                    <div class="arrow-bar arrow-bar1"></div>
+                    <div class="arrow-bar arrow-bar2"></div>
+                </button>
+                <div class="menu" id="menu">
+                    <a href="index.html">Home</a>    
+                    <a href="apaf.html">Alto Paraná Atlantic forests</a>
+                </div>
+            </div>
+            `;
+        }
+    }
+customElements.define('custom-navbar', NavbarComponent);
