3f6a197bcc3e419cf9c3244efe6a2a20760eb0dd
huaqo <mail@huaqo.com>
2023-12-19 15:51:09 +0100

    refactor

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ class NavbarComponent extends HTMLElement {
         }
     }
 customElements.define('custom-navbar', NavbarComponent);
+
+document.getElementById("Btn").addEventListener("click", function() {
+    let menu = document.getElementById("menu");
+    let arrow = document.getElementById("Btn");
+    
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "flex";
+        arrow.classList.add("active");
+    } else {
+        menu.style.display = "none";
+        arrow.classList.remove("active");
+    }
+});
